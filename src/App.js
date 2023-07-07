929ef2e74300d39d7db1bcaa51014a0151084339
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-07-07 18:13:08 +0200

    Add an animal on click
    Using useState, then map over the animals to render AnimalShow for each one, passing in the 'type' as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import AnimalShow from "./AnimalShow";
+import { useState } from "react";
 
 export default function App() {
+  // the state, and the setter function. Never update state without the setter function (never count = 3, only setCount(3))
+  const [animals, setAnimals] = useState([]);
+
+  function getRandomAnimal() {
+    const animals = ["bird", "cat", "cow", "dog", "horse"];
+    return animals[Math.floor(Math.random() * animals.length)];
+  }
+
+  // define the function (what you want to happen on the event)
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+    console.log(animals);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
     <div>
       <h1>Hello</h1>
-      <AnimalShow />
+      {/* pass in the event function on the event */}
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
