419534282f43ad8b3f551aabfe4b3a193709aa73
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-07-07 16:32:23 +0200

    Initial commit

---
+++ src/AnimalShow.js
@@ -0,0 +1,7 @@
+export default function AnimalShow() {
+  return (
+    <div>
+      <h3>Showing animals...</h3>
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import AnimalShow from "./AnimalShow";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <AnimalShow />
+    </div>
+  );
+}
